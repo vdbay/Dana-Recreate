d58146def6f41f37206fdb01aadcd7cc0a284f8d
Azhar Subhan <54460861+vdbay@users.noreply.github.com>
2022-06-22 17:19:37 +0700

    :recycle: refactor code
    Wrap Flutter with safearea

--- lib/app/widgets/homeScreen.dart
+++ lib/app/widgets/homeScreen.dart
@@ -8,38 +8,40 @@ class NadaHomeScreen extends StatelessWidget {
   const NadaHomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const customAppBar(
-          saldo: '2.726', backgroundColor: Color(0xFFFBC02D)),
-      body: ListView(scrollDirection: Axis.vertical, children: [
-        Stack(
-          alignment: AlignmentDirectional.topCenter,
-          children: [
-            Container(height: 2000, color: Colors.white),
-            const sendRecWidget(),
-            const Positioned.fill(top: 90, child: multiHomeWidget()),
-          ],
-        )
-      ]),
-      bottomNavigationBar: const bottomNavBar(),
-      floatingActionButton: Transform.scale(
-        scale: 1.3,
-        child: FloatingActionButton(
-          backgroundColor: Color(0xFFFBC02D),
-          onPressed: () {},
-          elevation: 5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(
-                Icons.qr_code_rounded,
-              ),
-              Text('PAY')
+    return SafeArea(
+      child: Scaffold(
+        appBar: const customAppBar(
+            saldo: '2.726', backgroundColor: Color(0xFFFBC02D)),
+        body: ListView(scrollDirection: Axis.vertical, children: [
+          Stack(
+            alignment: AlignmentDirectional.topCenter,
+            children: [
+              Container(height: 2000, color: Colors.white),
+              const sendRecWidget(),
+              const Positioned.fill(top: 90, child: multiHomeWidget()),
             ],
+          )
+        ]),
+        bottomNavigationBar: const bottomNavBar(),
+        floatingActionButton: Transform.scale(
+          scale: 1.3,
+          child: FloatingActionButton(
+            backgroundColor: Color(0xFFFBC02D),
+            onPressed: () {},
+            elevation: 5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.qr_code_rounded,
+                ),
+                Text('PAY')
+              ],
+            ),
           ),
         ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
